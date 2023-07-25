0b4354656930d8b04637c458bd34f367b6b4b5de
ATAKAN TEPE <tepe_atakan@outlook.com>
2023-07-25 13:40:46 +0300

    refactoring components' tailwind styles

--- plugins/primeVue.js
+++ plugins/primeVue.js
@@ -744,6 +744,57 @@ const tailwindLight = {
             ]
         })
     },
+    cascadeselect: {
+        root: ({ props }) => ({
+            class: [
+                'inline-flex cursor-pointer select-none relative',
+                'bg-white dark:bg-gray-900 border border-gray-300 dark:border-blue-900/40 transition duration-200 ease-in-out rounded-lg',
+                { 'opacity-60 select-none pointer-events-none cursor-default': props.disabled }
+            ]
+        }),
+        label: {
+            class: ['block whitespace-nowrap overflow-hidden flex flex-1 w-1 text-overflow-ellipsis cursor-pointer', 'bg-transparent border-0 p-3 text-gray-700 dark:text-white/80', 'appearance-none rounded-md']
+        },
+        dropdownbutton: {
+            class: ['flex items-center justify-center flex-shrink-0', 'bg-transparent text-gray-600 dark:text-white/80 w-[3rem] rounded-tr-6 rounded-br-6']
+        },
+        panel: {
+            class: ['absolute py-3 bg-white dark:bg-gray-900 border-0 shadow-md']
+        },
+        list: {
+            class: ['m-0 sm:p-0 list-none']
+        },
+        item: ({ props }) => ({
+            class: [
+                console.log(props),
+                'cursor-pointer font-normal whitespace-nowrap',
+                'm-0 border-0 bg-transparent transition-shadow rounded-none',
+                'text-gray-700 dark:text-white/80 hover:text-gray-700 dark:hover:text-white/80 hover:bg-gray-200 dark:hover:bg-gray-800/80'
+            ]
+        }),
+        content: {
+            class: ['flex items-center overflow-hidden relative', 'py-3 px-5']
+        },
+        groupicon: {
+            class: ['ml-auto']
+        },
+        sublist: {
+            class: ['block absolute left-full top-0', 'min-w-full z-10', 'py-3 bg-white dark:bg-gray-900 border-0 shadow-md']
+        }
+    },
+    inputgroup: {
+        root: {
+            class: ['flex items-stretch w-full flex-1']
+        }
+    },
+    inputmask: {
+        root: ({ props, context }) => ({
+            class: [
+                console.log(props),
+                'font-sans text-base text-gray-700 dark:text-white/80 bg-white dark:bg-gray-900 py-3 px-3 border border-gray-300 dark:border-blue-900/40 hover:border-blue-500 focus:outline-none focus:outline-offset-0 focus:shadow-[0_0_0_0.2rem_rgba(191,219,254,1)] dark:focus:shadow-[0_0_0_0.2rem_rgba(147,197,253,0.5)] transition duration-200 ease-in-out appearance-none rounded-md'
+            ]
+        })
+    },
     rating: {
         root: ({ props }) => ({
             class: ['relative flex items-center', 'gap-2', { 'opacity-60 select-none pointer-events-none cursor-default': props.disabled }]
@@ -995,39 +1046,39 @@ const tailwindLight = {
         }
     },
     calendar: {
-        root: {
-            class: ['inline-flex max-w-full relative']
-        },
+        root: ({ props }) => ({
+            class: ['inline-flex max-w-full relative', { 'opacity-60 select-none pointer-events-none cursor-default': props.disabled }]
+        }),
         input: {
-            class: ['font-sans text-base text-gray-600 bg-white p-3 border border-gray-300 transition-colors duration-200 appearance-none rounded-lg', 'hover:border-blue-500 ']
+            class: ['font-sans text-base text-gray-600 dark:text-white/80 bg-white dark:bg-gray-900 p-3 border border-gray-300 dark:border-blue-900/40 transition-colors duration-200 appearance-none rounded-lg', 'hover:border-blue-500 ']
         },
-        panel: {
-            class: ['bg-white border-0 shadow-md', 'absolute min-w-[350px]']
-        },
+        panel: ({ props }) => ({
+            class: ['bg-white dark:bg-gray-900', 'min-w-[350px]', { 'shadow-md border-0 absolute': !props.inline, 'inline-block overflow-x-auto border border-gray-300 dark:border-blue-900/40 p-2 rounded-lg': props.inline }]
+        }),
         header: {
-            class: ['flex items-center justify-between', 'p-2 text-gray-700 bg-white font-semibold m-0 border-b border-gray-300 rounded-t-lg']
+            class: ['flex items-center justify-between', 'p-2 text-gray-700 dark:text-white/80 bg-white dark:bg-gray-900 font-semibold m-0 border-b border-gray-300 dark:border-blue-900/40 rounded-t-lg']
         },
         previousbutton: {
             class: [
                 'flex items-center justify-center cursor-pointer overflow-hidden relative',
-                'w-8 h-8 text-gray-600 border-0 bg-transparent rounded-full transition-colors duration-200 ease-in-out',
-                'hover:text-gray-700 hover:border-transparent hover:bg-gray-200'
+                'w-8 h-8 text-gray-600 dark:text-white/70 border-0 bg-transparent rounded-full transition-colors duration-200 ease-in-out',
+                'hover:text-gray-700 dark:hover:text-white/80 hover:border-transparent hover:bg-gray-200 dark:hover:bg-gray-800/80 '
             ]
         },
         title: {
             class: ['leading-8 mx-auto']
         },
         monthTitle: {
-            class: ['text-gray-700 transition duration-200 font-semibold p-2', 'mr-2', 'hover:text-blue-500']
+            class: ['text-gray-700 dark:text-white/80 transition duration-200 font-semibold p-2', 'mr-2', 'hover:text-blue-500']
         },
         yearTitle: {
-            class: ['text-gray-700 transition duration-200 font-semibold p-2', 'hover:text-blue-500']
+            class: ['text-gray-700 dark:text-white/80 transition duration-200 font-semibold p-2', 'hover:text-blue-500']
         },
         nextbutton: {
             class: [
                 'flex items-center justify-center cursor-pointer overflow-hidden relative',
-                'w-8 h-8 text-gray-600 border-0 bg-transparent rounded-full transition-colors duration-200 ease-in-out',
-                'hover:text-gray-700 hover:border-transparent hover:bg-gray-200'
+                'w-8 h-8 text-gray-600 dark:text-white/70 border-0 bg-transparent rounded-full transition-colors duration-200 ease-in-out',
+                'hover:text-gray-700 dark:hover:text-white/80 hover:border-transparent hover:bg-gray-200 dark:hover:bg-gray-800/80 '
             ]
         },
         table: {
@@ -1036,11 +1087,77 @@ const tailwindLight = {
         tableheadercell: {
             class: ['p-2']
         },
+        weekday: {
+            class: ['text-gray-600 dark:text-white/70']
+        },
         day: {
             class: ['p-2']
         },
-        daylabel: {
-            class: ['w-10 h-10 rounded-full transition-shadow duration-200 border-transparent border', 'flex items-center cursor-pointer justify-center mx-auto overflow-hidden relative']
+        daylabel: ({ props }) => ({
+            class: [
+                console.log(props),
+                'w-10 h-10 rounded-full transition-shadow duration-200 border-transparent border dark:text-white/80',
+                'flex items-center cursor-pointer justify-center mx-auto overflow-hidden relative',
+                'hover:bg-gray-200 dark:hover:bg-gray-800/80  focus:outline-none focus:outline-offset-0 focus:shadow-[0_0_0_0.2rem_rgba(191,219,254,1)] dark:focus:shadow-[0_0_0_0.2rem_rgba(147,197,253,0.5)]'
+            ]
+        }),
+        monthpicker: {
+            class: ['my-2']
+        },
+        month: {
+            class: [
+                'w-1/3 inline-flex items-center justify-center cursor-pointer overflow-hidden relative dark:text-white/80',
+                'p-2 transition-shadow duration-200 rounded-lg',
+                'hover:bg-gray-200 dark:hover:bg-gray-800/80  focus:outline-none focus:outline-offset-0 focus:shadow-[0_0_0_0.2rem_rgba(191,219,254,1)] dark:focus:shadow-[0_0_0_0.2rem_rgba(147,197,253,0.5)]'
+            ]
+        },
+        yearpicker: {
+            class: ['my-2']
+        },
+        year: {
+            class: [
+                'w-1/2 inline-flex items-center justify-center cursor-pointer overflow-hidden relative dark:text-white/80',
+                'p-2 transition-shadow duration-200 rounded-lg',
+                'hover:bg-gray-200 dark:hover:bg-gray-800/80  focus:outline-none focus:outline-offset-0 focus:shadow-[0_0_0_0.2rem_rgba(191,219,254,1)] dark:focus:shadow-[0_0_0_0.2rem_rgba(147,197,253,0.5)]'
+            ]
+        },
+        timepicker: {
+            class: ['flex justify-center items-center', 'border-t-1 border-solid border-gray-300 p-2']
+        },
+        separatorcontainer: {
+            class: ['flex items-center flex-col px-2']
+        },
+        separator: {
+            class: ['text-xl']
+        },
+        hourpicker: {
+            class: ['flex items-center flex-col px-2']
+        },
+        minutepicker: {
+            class: ['flex items-center flex-col px-2']
+        },
+        ampmpicker: {
+            class: ['flex items-center flex-col px-2']
+        },
+        incrementbutton: {
+            class: [
+                'flex items-center justify-center cursor-pointer overflow-hidden relative',
+                'w-8 h-8 text-gray-600 dark:text-white/70 border-0 bg-transparent rounded-full transition-colors duration-200 ease-in-out',
+                'hover:text-gray-700 dark:hover:text-white/80 hover:border-transparent hover:bg-gray-200 dark:hover:bg-gray-800/80 '
+            ]
+        },
+        decrementbutton: {
+            class: [
+                'flex items-center justify-center cursor-pointer overflow-hidden relative',
+                'w-8 h-8 text-gray-600 dark:text-white/70 border-0 bg-transparent rounded-full transition-colors duration-200 ease-in-out',
+                'hover:text-gray-700 dark:hover:text-white/80 hover:border-transparent hover:bg-gray-200 dark:hover:bg-gray-800/80 '
+            ]
+        },
+        groupcontainer: {
+            class: ['flex']
+        },
+        group: {
+            class: ['flex-1', 'border-l border-gray-300 pr-0.5 pl-0.5 pt-0 pb-0', 'first:pl-0 first:border-l-0']
         }
     },
     listbox: {
