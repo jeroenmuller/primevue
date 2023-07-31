faaadc18a430d4f3c908e0bc31b24e2c83a14183
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2023-07-31 11:37:51 +0300

    Tailwind Tooltip updates

--- plugins/primeVue.js
+++ plugins/primeVue.js
@@ -127,12 +127,29 @@ const tailwindLight = {
             })
         },
         tooltip: {
-            root: {
-                class: ['absolute bg-gray-700 text-white p-3 shadow-md rounded-md']
-            },
-            arrow: ({ props }) => ({
-                class: [console.log(props), 'border-r border-gray-700']
-            })
+            root: ({ context }) => ({
+                class: [
+                    'absolute shadow-md',
+                    {
+                        'py-0 px-1': context?.right || context?.left || (!context?.right && !context?.left && !context?.top && !context?.bottom),
+                        'py-1 px-0': context?.top || context?.bottom
+                    }
+                ]
+            }),
+            arrow: ({ context }) => ({
+                class: [
+                    'absolute w-0 h-0 border-transparent border-solid',
+                    {
+                        '-m-t-1 border-y-[0.25rem] border-r-[0.25rem] border-l-0 border-r-gray-600': context?.right || (!context?.right && !context?.left && !context?.top && !context?.bottom),
+                        '-m-t-1 border-y-[0.25rem] border-l-[0.25rem] border-r-0 border-l-gray-600': context?.left,
+                        '-m-l-1 border-x-[0.25rem] border-t-[0.25rem] border-b-0 border-t-gray-600': context?.top,
+                        '-m-l-1 border-x-[0.25rem] border-b-[0.25rem] border-t-0 border-b-gray-600': context?.bottom
+                    }
+                ]
+            }),
+            text: {
+                class: 'p-3 bg-gray-600 text-white rounded-md whitespace-pre-line break-words'
+            }
         }
     },
     //PANELS
