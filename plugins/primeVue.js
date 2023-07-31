13bfb1736db6302f1ef8989356887e12828dbaae
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2023-07-31 15:11:11 +0300

    Tailwind Badge directive updates

--- plugins/primeVue.js
+++ plugins/primeVue.js
@@ -115,14 +115,23 @@ const tailwindLight = {
             }
         },
         badge: {
-            root: ({ instance }) => ({
+            root: ({ context }) => ({
                 class: [
-                    'rounded-full p-0 text-center absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2 origin-top-right m-0',
-                    'p-0 rounded-full',
-                    'text-xsmin-w-6 h-6 flex items-center justify-center',
-                    'text-center inline-block',
-                    'bg-blue-500 text-white font-bold',
-                    console.log(instance)
+                    'absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2 origin-top-right m-0',
+                    'text-xs leading-6 flex items-center justify-center',
+                    'text-center text-white font-bold',
+                    {
+                        'rounded-full p-0': context.nogutter || context.dot,
+                        'rounded-[10px] px-2': !context.nogutter && !context.dot,
+                        'min-w-[0.5rem] w-2 h-2': context.dot,
+                        'min-w-[1.5rem] h-6': !context.dot
+                    },
+                    {
+                        'bg-blue-500 ': context.info || (!context.info && !context.success && !context.warning && !context.danger),
+                        'bg-green-500 ': context.success,
+                        'bg-orange-500 ': context.warning,
+                        'bg-red-500 ': context.danger
+                    }
                 ]
             })
         },
