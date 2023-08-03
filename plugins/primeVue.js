c600f8e2163e996ee1fa00a3d84b2d245acf6d98
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2023-08-03 16:43:38 +0300

    Dialog fixes

--- plugins/primeVue.js
+++ plugins/primeVue.js
@@ -520,8 +520,44 @@ const tailwindLight = {
         footer: {
             class: ['shrink-0 ', 'border-t-0 bg-white text-gray-700 px-6 pb-6 text-right rounded-b-lg', 'dark:bg-gray-900  dark:text-white/80']
         },
-        mask: {
-            class: ['bg-black/40 transition duration-200']
+        mask: ({ props }) => ({
+            class: ['transition duration-200', { 'bg-black/40': props.modal }]
+        }),
+        transition: ({ props }) => {
+            return props.position === 'top'
+                ? {
+                      enterFromClass: 'opacity-0 scale-75 translate-x-0 -translate-y-full translate-z-0',
+                      enterActiveClass: 'transition-all duration-200 ease-out',
+                      leaveActiveClass: 'transition-all duration-200 ease-out',
+                      leaveToClass: 'opacity-0 scale-75 translate-x-0 -translate-y-full translate-z-0'
+                  }
+                : props.position === 'bottom'
+                ? {
+                      enterFromClass: 'opacity-0 scale-75 translate-y-full',
+                      enterActiveClass: 'transition-all duration-200 ease-out',
+                      leaveActiveClass: 'transition-all duration-200 ease-out',
+                      leaveToClass: 'opacity-0 scale-75 translate-x-0 translate-y-full translate-z-0'
+                  }
+                : props.position === 'left' || props.position === 'topleft' || props.position === 'bottomleft'
+                ? {
+                      enterFromClass: 'opacity-0 scale-75 -translate-x-full translate-y-0 translate-z-0',
+                      enterActiveClass: 'transition-all duration-200 ease-out',
+                      leaveActiveClass: 'transition-all duration-200 ease-out',
+                      leaveToClass: 'opacity-0 scale-75  -translate-x-full translate-y-0 translate-z-0'
+                  }
+                : props.position === 'right' || props.position === 'topright' || props.position === 'bottomright'
+                ? {
+                      enterFromClass: 'opacity-0 scale-75 translate-x-full translate-y-0 translate-z-0',
+                      enterActiveClass: 'transition-all duration-200 ease-out',
+                      leaveActiveClass: 'transition-all duration-200 ease-out',
+                      leaveToClass: 'opacity-0 scale-75 opacity-0 scale-75 translate-x-full translate-y-0 translate-z-0'
+                  }
+                : {
+                      enterFromClass: 'opacity-0 scale-75',
+                      enterActiveClass: 'transition-all duration-200 ease-out',
+                      leaveActiveClass: 'transition-all duration-200 ease-out',
+                      leaveToClass: 'opacity-0 scale-75'
+                  };
         }
     },
     confirmpopup: {
