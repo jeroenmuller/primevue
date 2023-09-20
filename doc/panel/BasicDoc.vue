389397141d44383c5b7c303c45b1e50cf1527fd6
mertsincan <sincan.mert@gmail.com>
2023-09-20 11:26:20 +0100

    Panel tests

--- doc/panel/BasicDoc.vue
+++ doc/panel/BasicDoc.vue
@@ -3,7 +3,13 @@
         <p>A simple Panel is created with a <i>header</i> property along with the content as children.</p>
     </DocSectionText>
     <div class="card">
-        <Panel header="Header">
+        <div class="flex justify-content-center gap-3 mb-3">
+            <div class="w-1rem h-1rem border-1 border-circle cursor-pointer" style="background: var(--p-panel-header-background)" @click="changeHeaderBackground()"></div>
+            <div class="w-1rem h-1rem border-1 border-circle cursor-pointer" style="background: var(--p-blue-500)" @click="changeHeaderBackground('blue')"></div>
+            <div class="w-1rem h-1rem border-1 border-circle cursor-pointer" style="background: var(--p-green-500)" @click="changeHeaderBackground('green')"></div>
+            <div class="w-1rem h-1rem border-1 border-circle cursor-pointer" style="background: var(--p-yellow-500)" @click="changeHeaderBackground('yellow')"></div>
+        </div>
+        <Panel header="Header" :style="theme">
             <p class="m-0">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
@@ -14,6 +20,8 @@
 </template>
 
 <script>
+import PrimeCSS from '@/components/lib/usecss/primecss';
+
 export default {
     data() {
         return {
@@ -50,8 +58,26 @@ export default {
 
 <script setup>
 <\/script>`
-            }
+            },
+            theme: undefined
         };
+    },
+    methods: {
+        changeHeaderBackground(color) {
+            if (color) {
+                this.theme = PrimeCSS.toVariables({
+                    panel: {
+                        header: {
+                            properties: {
+                                background: `var(--p-${color}-500)`
+                            }
+                        }
+                    }
+                }).value.join('');
+            } else {
+                this.theme = undefined;
+            }
+        }
     }
 };
 </script>
--- doc/panel/ToggleableDoc.vue
+++ doc/panel/ToggleableDoc.vue
@@ -3,7 +3,7 @@
         <p>Content of the panel can be expanded and collapsed using <i>toggleable</i> option.</p>
     </DocSectionText>
     <div class="card">
-        <Panel header="Header" toggleable>
+        <Panel header="Header" toggleable class="custom-panel">
             <p class="m-0">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
@@ -14,6 +14,10 @@
 </template>
 
 <script>
+import { useCSS } from 'primevue/usecss';
+
+const { load: loadCSS } = useCSS('custom-panel', { selectorOptions: { selectors: { 'custom-panel': '.custom-panel' } } });
+
 export default {
     data() {
         return {
@@ -52,6 +56,17 @@ export default {
 <\/script>`
             }
         };
+    },
+    mounted() {
+        loadCSS({
+            'custom-panel': {
+                title: {
+                    properties: {
+                        color: 'red'
+                    }
+                }
+            }
+        });
     }
 };
 </script>
