c8945b3734fddddca28ef98aeaa1ca08af0357ae
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2023-10-17 11:50:01 +0300

    Updates

--- plugins/primeVue.js
+++ plugins/primeVue.js
@@ -1,10 +1,11 @@
 import PrimeVue from '@/components/lib/config/PrimeVue';
+import Tailwind from '@/components/lib/passthrough/tailwind/index.js';
 
 import Animate from '@/components/lib/animate/Animate';
 import CodeHighlight from '@/directives/CodeHighlight';
 
 export default defineNuxtPlugin((nuxtApp) => {
-    nuxtApp.vueApp.use(PrimeVue, { ripple: true, unstyled: true, pt: CustomTailwind });
+    nuxtApp.vueApp.use(PrimeVue, { ripple: true, unstyled: true, pt: Tailwind });
 
     nuxtApp.vueApp.directive('animate', Animate);
     nuxtApp.vueApp.directive('code', CodeHighlight);
